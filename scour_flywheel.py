b59a9430440f3826deafa47523b7bd3739e87386
Avi Kelman <patcherton.fixesthings@gmail.com>
2021-09-22 15:22:47 -0400

    :recycle::zap: Switch to pure View requests
    This is much faster than iterating finders

--- scour_flywheel.py
+++ scour_flywheel.py
@@ -4,7 +4,7 @@ and upload the records to the warehouse DB.
 """
 import os
 import re
-
+import json
 import flywheel
 import pandas
 from sqlalchemy import create_engine
@@ -18,82 +18,50 @@ fw = flywheel.Client(fw_api_token)
 db = create_engine(db_url)
 table = "flywheel_export"
 
-# Get metadata quickly with a View. This is very fast. No acquisitions or files
-# here, though, because those seem to break the view requester.
+# Get file metadata quickly with Views. This is relatively fast.
 
-with yaspin(text="Fetching flywheel View data.") as spin:
-    view = fw.View(
-        columns=["project.label", "subject.label", "session.label", "session.id"],
-        include_ids=False,
-    )
-    # FIXME: I've replaced read_view_dataframe for now because the current
-    # implementation (as of 16.0.0rc1) raises a FutureWarning from pandas
-    # because it passes "records" as a positional argument instead of the
-    # orient keyword argument. Please periodically check the implementation of
-    # read_view_dataframe to see if flywheel.io has fixed this. - Avi
-    meta_df = pandas.concat(
-        (
-            pandas.read_json(
-                fw.read_view_data(view, p.id, decode=False, format="json-flat"),
-                orient="records",
-            )
-            for p in fw.projects.iter()
-        ),
-        ignore_index=True,
-    )
-    # meta_df = pandas.concat(
-    #     (fw.read_view_dataframe(view, p.id) for p in fw.projects.iter()),
-    #     ignore_index=True,
-    # )
-    spin.ok("✅")
+view = fw.View(
+    container="acquisition",
+    filename="*",
+    match="all",
+    columns=[
+        "file.name",
+        "file.size",
+        "file.modality",
+        "file.info.MagneticFieldStrength",
+        "file.classification.Intent",
+        "file.classification.Features",
+        "file.classification.Measurement",
+        "file.created",
+        "file.modified",
+    ],
+    include_ids=True,
+    include_labels=True,
+    process_files=False,
+    sort=False,
+)
 
-# Get acquisition file details with a find iterator. Not super fast, but
-# somehow infinitely faster than including them in the View.
 
-msg = "Scouring flywheel for acquisition files."
-with yaspin(text=msg) as spin:
-    files = []
-    file_cols = (
-        "id",
-        "name",
-        "size",
-        "modality",
-        "created",
-        "modified",
-        "classification.Intent",
-        "classification.Features",
-        "classification.Measurement",
-        "info.MagneticFieldStrength",
-    )
-
-    def burrow(what, where):
-        for level in f"_{where}".split("."):
-            what = what.get(level)
-        return what
-
-    # Using an iter_find filter for "modification>..." is not safe here because
-    # attached files can be newer than the acquisition's timestamp.
-    for ac in fw.acquisitions.iter():
-        session = {"session.id": ac.session}
-        for f in ac.files:
-            fv = vars(f)
-            files.append(session | {f"file_{c}": burrow(fv, c) for c in file_cols})
-            spin.text = f"{msg}  Found: {len(files)}"
-
-    spin.ok("✅")
+all_data = []
+for project in fw.projects.iter():
+    pid = project.id
+    name = project.label
+    with yaspin(text=f"Fetching view for project {name} ({pid})...") as spin:
+        d = json.load(fw.read_view_data(view, pid, decode=False, format="json-flat"))
+        all_data.extend(d)
+        spin.text = f"Found {len(d)} files for project {name} ({pid})."
+        spin.ok("✅")
 
 # Send file records to database.
 
-if files:
-    with yaspin(text=f"Submitting {len(files)} records to the database...") as spin:
-        rex = re.compile(r"(?<!_)(?=[A-Z])")
-        df = (
-            pandas.DataFrame(files)
-            .merge(meta_df, how="left", on="session.id")
-            .drop(columns=["session.id"])  # we don't need this anymore
-            .rename(columns=lambda x: x.replace(".", "_"))  # avoid "." in cols
-            .rename(columns=lambda x: rex.sub('_', x).lower()) # snake to camel
-        )
+if all_data:
+    rex = re.compile(r"(?<!_)(?=[A-Z])")
+    df = (
+        pandas.DataFrame(all_data)
+        .rename(columns=lambda x: x.replace(".", "_"))  # avoid "." in colnames
+        .rename(columns=lambda x: rex.sub("_", x).lower())  # camelcase to snake
+    )
+    with yaspin(text=f"Submitting {len(df)} records to the database...") as spin:
         df.to_sql(
             table, db, index=False, if_exists="replace", chunksize=10000, method="multi"
         )
