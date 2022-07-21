1f04b37ab5dddd090d8d036d7627418cb49d1b19
Ariana <arianafamiliar@hotmail.com>
2022-07-21 09:49:18 -0400

    Removed yaspin for Jenkins log readability

--- scour_flywheel.py
+++ scour_flywheel.py
@@ -8,7 +8,6 @@ import json
 import flywheel
 import pandas
 from sqlalchemy import create_engine
-from yaspin import yaspin
 
 fw_api_token = os.getenv("FLYWHEEL_API_TOKEN")
 db_url = os.getenv("D3B_WAREHOUSE_DB_URL")
@@ -45,14 +44,13 @@ view = fw.View(
 
 
 all_data = []
+# for project in fw.projects.iter():
 for project in fw.projects.iter():
     pid = project.id
     name = project.label
-    with yaspin(text=f"Fetching view for project {name} ({pid})...") as spin:
-        d = json.load(fw.read_view_data(view, pid, decode=False, format="json-flat"))
-        all_data.extend(d)
-        spin.text = f"Found {len(d)} files for project {name} ({pid})."
-        spin.ok("✅")
+    print(f"Fetching view for project {name} ({pid})...")
+    d = json.load(fw.read_view_data(view, pid, decode=False, format="json-flat"))
+    all_data.extend(d)
 
 # Send file records to database.
 
@@ -63,14 +61,11 @@ if all_data:
         .rename(columns=lambda x: x.replace(".", "_"))  # avoid "." in colnames
         .rename(columns=lambda x: rex.sub("_", x).lower())  # camelcase to snake
     )
-    with yaspin(
-        text=f"Submitting {len(df)} records to the '{table}' table in {repr(db.url)}..."
-    ) as spin:
-        df.to_sql(
-            table, db, index=False, if_exists="replace", chunksize=10000, method="multi"
-        )
-        with db.connect() as conn:
-            conn.execute(f"GRANT SELECT ON {table} TO public")
-        spin.ok("✅")
+    print(f"Submitting {len(df)} records to the '{table}' table in {repr(db.url)}...")
+    df.to_sql(
+        table, db, index=False, if_exists="replace", chunksize=10000, method="multi"
+    )
+    with db.connect() as conn:
+        conn.execute(f"GRANT SELECT ON {table} TO public")
 else:
     print("No files found.")
